4d4b5f3b75e9aacc544645788d484a0ccb8dac04
Nick De Raj <nickderaj@gmail.com>
2021-06-29 14:17:58 +0800

    Adding usernames

--- script.js
+++ script.js
@@ -56,3 +56,15 @@ const inputTransferAmount = document.querySelector(".form__input--amount");
 const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
+
+// Functions
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((name) => name[0])
+      .join("");
+  });
+};
+createUsernames(accounts);
